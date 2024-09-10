7b8570084696bcc93092992d1cc704de953b781f
Elias <eli1341994@gmail.com>
2024-09-10 16:27:24 -0300

    Del sveltekit initial boilerplate

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -11,7 +11,7 @@
 	</main>
 
 	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+		<p>Creative pratices by <strong>Ellie Broocks</strong></p>
 	</footer>
 </div>
 
