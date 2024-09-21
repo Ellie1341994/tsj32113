9a12428e500c2cd63e8e221e2f9706af04c7e0bf
Elias <eli1341994@gmail.com>
2024-09-20 22:18:00 -0300

    Fix basic lessons title page number

--- app/src/routes/tsj/basics/+layout.svelte
+++ app/src/routes/tsj/basics/+layout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	let lesson = $page.params.page;
 </script>
 
 <svelte:head>
@@ -8,6 +7,7 @@
 		<title>TSJ. Basic Techniques</title>
 		<meta name="Basics" content="Example set of basic techniques regarding ThreeJS" />
 	{:else}
+		{@const lesson = $page.params.page}
 		<title>TSJ. {lesson}</title>
 		<meta name="Basics lesson {lesson}" content="Lesson {lesson} code" />
 	{/if}
