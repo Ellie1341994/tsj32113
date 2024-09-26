dc06d39bdecd2eabcccede2f66446979fd753df0
Elias <eli1341994@gmail.com>
2024-09-26 14:12:20 -0300

    Add svelte directive for base transition from basics route

--- app/src/routes/tsj/basics/+layout.svelte
+++ app/src/routes/tsj/basics/+layout.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import { fade, fly } from 'svelte/transition';
+	console.log('checking for duplicated execution: basics');
 </script>
 
 {#key $page.params.page}
-	<slot />
+	<span in:fade={{ delay: 1000, duration: 1000 }} out:fade={{ duration: 500 }}><slot /></span>
 {/key}
