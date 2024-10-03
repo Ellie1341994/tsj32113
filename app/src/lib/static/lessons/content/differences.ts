ff5c76c4c7e8b3e7a8f7aa7819433b31e2b0e61f
Elias <eli1341994@gmail.com>
2024-10-03 17:06:48 -0300

    Add initial files for decoupling static content

---
+++ app/src/lib/static/lessons/content/differences.ts
@@ -0,0 +1,8 @@
+import lessonTitles from "$lib/static/lessons/content/titles";
+
+export const DIFFERENCES: any = {}
+const lesson14 = lessonTitles[13]
+DIFFERENCES[lesson14] = {
+    1: "Lights Color palette", 
+    2: "Animated Point Light", 
+    3: "Custom Camera position"}
