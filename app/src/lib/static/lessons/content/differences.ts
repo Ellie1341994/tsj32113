efce23f96fbe65da76be1fb55b77f2a9af4ec2a3
Elias <eli1341994@gmail.com>
2024-10-04 11:05:19 -0300

    Add static content lesson 15 and 16 differences

--- app/src/lib/static/lessons/content/differences.ts
+++ app/src/lib/static/lessons/content/differences.ts
@@ -6,3 +6,16 @@ DIFFERENCES[lesson14] = {
     1: "Lights Color palette", 
     2: "Animated Point Light", 
     3: "Custom Camera position"}
+const lesson15 = lessonTitles[13]
+DIFFERENCES[lesson15] = {
+    1: "Animated plane on z axis", 
+    2: "Custom color axes helper",
+    3: "Custom camera position",}
+const lesson16 = lessonTitles[13]
+    DIFFERENCES[lesson16] = {
+        1: "Roof Animated and pyramid-shaped", 
+        2: "Custom color sky",
+        3: "On demand graves levitation by pressing danw the space key or the h key and manually clicking the animation button on the panel",
+        4: "Custom door texture",
+        5: "Custom grave sizes and placement",
+    }
