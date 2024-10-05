0d43d58041fe1811fab4661e3879fa40653788ac
Elias <eli1341994@gmail.com>
2024-10-05 20:43:02 -0300

    Add lesson 18 component

--- app/src/lib/classic_techniques/18.svelte
+++ app/src/lib/classic_techniques/18.svelte
@@ -4,13 +4,196 @@
 	// BIG NOTE: REMAINING MEMORY ALLOCATED SEEMS TO BE INTERNAT TO THREE.JS
 	// import gsap from `gsap`;
 	import GUI from 'lil-gui';
-	import { onMount } from 'svelte';
+	import { onMount, tick } from 'svelte';
 	import * as THREE from 'three';
 	import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 	import { Sky } from 'three/addons/objects/Sky.js';
 	import { Timer } from 'three/addons/misc/Timer.js';
 	let canvas: HTMLCanvasElement;
 	let lilGuiPlacer: HTMLSpanElement;
+	onMount(() => {
+		// Other features
+		// Feature: Fullscreen
+		// Size fix on toggle off
+		const setCanvasSize = () => {
+			// Update camera
+			camera.updateProjectionMatrix();
+
+			// Update renderer
+			renderer.setSize(sizes.width, sizes.height);
+			renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+		};
+		window.addEventListener(`resize`, setCanvasSize);
+		// Full screen support
+		const toggleFullscreen = () => {
+			document.fullscreenElement ? document.exitFullscreen() : canvas?.requestFullscreen();
+		};
+		window.addEventListener(`dblclick`, toggleFullscreen);
+		// Teaks panel
+		const gui = new GUI({
+			width: 300,
+			container: lilGuiPlacer,
+			closeFolders: true,
+			title: 'Galaxy Teaks'
+		});
+		// Constants
+		const sizes = { width: window.innerWidth * 0.75, height: window.innerHeight * 0.75 };
+		const ASPECT_RATIO = sizes.width / sizes.height;
+		// Scene
+		const scene = new THREE.Scene();
+
+		// Meshes
+		const cube = new THREE.Mesh(
+			new THREE.BoxGeometry(1, 1),
+			new THREE.MeshBasicMaterial({ color: '0x696969' })
+		);
+		// Galaxy Gen
+		const parameters = {
+			count: 100000,
+			size: 0.01,
+			radius: 5,
+			branches: 3,
+			spin: 3,
+			randomness: 0.2,
+			randomnessPower: 3,
+			insideColor: '#ff6030',
+			outsideColor: '#1b3984'
+		};
+		let geometry: any = null;
+		let material: any = null;
+		let points: any = null;
+		function generateGalaxy() {
+			if (points) {
+				geometry.dispose();
+				material.dispose();
+				scene.remove(points);
+				console.log('Previously generated galaxy disposed?', renderer.info);
+			}
+			geometry = new THREE.BufferGeometry();
+			const dimensions = 3;
+			const vertices = new Float32Array(parameters.count * dimensions);
+			const colors = new Float32Array(parameters.count * dimensions);
+			for (let p = 0; p < parameters.count; p++) {
+				const p3 = p * dimensions;
+				const radius = Math.random() * parameters.radius;
+				const branchAngle = ((p % parameters.branches) / parameters.branches) * Math.PI * 2;
+				const spinAngle = parameters.spin * radius;
+
+				const randomX =
+					Math.pow(Math.random(), parameters.randomnessPower) *
+					(Math.random() < 0.5 ? 1 : -1) *
+					parameters.randomness *
+					radius;
+				const randomY =
+					Math.pow(Math.random(), parameters.randomnessPower) *
+					(Math.random() < 0.5 ? 1 : -1) *
+					parameters.randomness *
+					radius;
+				const randomZ =
+					Math.pow(Math.random(), parameters.randomnessPower) *
+					(Math.random() < 0.5 ? 1 : -1) *
+					parameters.randomness *
+					radius;
+				// all x
+				vertices[p3] = Math.cos(branchAngle + spinAngle) * radius + randomX;
+				// all y
+				vertices[p3 + 1] = randomY;
+				// all z
+				vertices[p3 + 2] = Math.sin(branchAngle + spinAngle) * radius + randomZ;
+
+				// Colors
+
+				const colorInside = new THREE.Color(parameters.insideColor);
+				const colorOutside = new THREE.Color(parameters.outsideColor);
+				const mixedColor = colorInside.clone();
+				mixedColor.lerp(colorOutside, radius / parameters.radius);
+				colors[p3] = mixedColor.r;
+				colors[p3 + 1] = mixedColor.g;
+				colors[p3 + 2] = mixedColor.b;
+			}
+			geometry.setAttribute('position', new THREE.BufferAttribute(vertices, dimensions));
+			geometry.setAttribute('color', new THREE.BufferAttribute(colors, dimensions));
+
+			material = new THREE.PointsMaterial({
+				size: parameters.size,
+				sizeAttenuation: true,
+				depthWrite: false,
+				blending: THREE.AdditiveBlending,
+				vertexColors: true
+			});
+
+			points = new THREE.Points(geometry, material);
+			scene.add(points);
+			gsap.fromTo(
+				points.rotation,
+				{ y: points.rotation },
+				{ y: 9, duration: 6, repeat: -1, yoyo: true }
+			);
+		}
+		// Organization
+		// scene.add(cube);
+		generateGalaxy();
+		gui.add(parameters, 'count', 100, 100000, 100).name('Particles').onFinishChange(generateGalaxy);
+		gui.add(parameters, 'size', 0.001, 0.1, 0.001).name('P. Size').onFinishChange(generateGalaxy);
+		gui.add(parameters, 'radius', 0.01, 20, 0.01).name('P. Radius').onFinishChange(generateGalaxy);
+		gui
+			.add(parameters, 'branches', 1, 20, 1)
+			.name('Galaxy. Branches')
+			.onFinishChange(generateGalaxy);
+		gui.add(parameters, 'spin', -5, 5, 0.001).name('P. Spin').onFinishChange(generateGalaxy);
+		gui
+			.add(parameters, 'randomness', 0, 2, 0.001)
+			.name('P. Randomness')
+			.onFinishChange(generateGalaxy);
+		gui
+			.add(parameters, 'randomnessPower', 1, 10, 0.001)
+			.name('P. Rand. Power')
+			.onFinishChange(generateGalaxy);
+
+		gui.addColor(parameters, 'insideColor').onFinishChange(generateGalaxy).name('G. insideColor');
+		gui.addColor(parameters, 'outsideColor').onFinishChange(generateGalaxy).name('G. outsideColor');
+
+		// Cam
+		const camera = new THREE.PerspectiveCamera(75, ASPECT_RATIO);
+		camera.position.z = 7;
+		camera.position.y = 6;
+		const control = new OrbitControls(camera, canvas);
+		// Renderer
+		const renderer = new THREE.WebGLRenderer({ canvas });
+		renderer.setSize(sizes.width, sizes.height);
+
+		renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+		// Play
+
+		let tickId: number = 0;
+		function tick() {
+			control.update();
+			renderer.render(scene, camera);
+			tickId = window.requestAnimationFrame(tick);
+		}
+		tick();
+
+		// Resources Management
+		function disposeScene() {
+			geometry.dispose();
+			material.dispose();
+			scene.clear();
+			scene.removeFromParent();
+			control.dispose();
+			console.log(`disposed first project allocated resources`, renderer.info);
+			gui.destroy();
+			console.log(`GUI destroyed`);
+			console.log(`tickId`, tickId);
+			window.cancelAnimationFrame(tickId);
+			window.removeEventListener(`resize`, setCanvasSize);
+			window.removeEventListener(`dbclick`, toggleFullscreen);
+			console.log(`Tick disposed`);
+			renderer.clear();
+			renderer.dispose();
+			console.log(`Renderer cleared and`);
+		}
+		return disposeScene;
+	});
 </script>
 
 <canvas class="webgl" bind:this={canvas}></canvas>
