2ba416330a2ff0b685ca8bb97c3b68786c83ea72
Elias <eli1341994@gmail.com>
2024-10-06 11:57:15 -0300

    Add lesson 18 differences content

--- app/src/lib/static/lessons/content/differences.ts
+++ app/src/lib/static/lessons/content/differences.ts
@@ -10,18 +10,24 @@ const lesson15 = lessonTitles[14]
 DIFFERENCES[lesson15] = {
     1: "Animated plane on z axis", 
     2: "Custom color axes helper",
-    3: "Custom camera position",}
+    3: "Custom camera position",
+}
 const lesson16 = lessonTitles[15]
-    DIFFERENCES[lesson16] = {
+DIFFERENCES[lesson16] = {
         1: "Roof Animated and pyramid-shaped", 
         2: "Custom color sky",
         3: "On demand graves levitation by pressing dawn the space key or the h key and manually clicking the animation button on the panel",
         4: "Custom door texture",
         5: "Custom grave sizes and placement",
-    }
-    const lesson17 = lessonTitles[16]
-    DIFFERENCES[lesson17] = {
+}
+const lesson17 = lessonTitles[16]
+DIFFERENCES[lesson17] = {
         1: "Wave Animated on X axes and mantained the rotation ", 
         2: "Yellow colored",
         3: "Tweaks panel",
-    }
+}
+const lesson18 = lessonTitles[17]
+DIFFERENCES[lesson18] = {
+        1: "Points animated on Y axis", 
+        2: "Higher spin for a fuller galaxy",
+}
