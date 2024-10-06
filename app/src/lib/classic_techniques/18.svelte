b087d571be9ab0f449b70180308d12880c713766
Elias <eli1341994@gmail.com>
2024-10-06 11:56:45 -0300

    Remove never read imports

--- app/src/lib/classic_techniques/18.svelte
+++ app/src/lib/classic_techniques/18.svelte
@@ -4,11 +4,9 @@
 	// BIG NOTE: REMAINING MEMORY ALLOCATED SEEMS TO BE INTERNAT TO THREE.JS
 	// import gsap from `gsap`;
 	import GUI from 'lil-gui';
-	import { onMount, tick } from 'svelte';
+	import { onMount } from 'svelte';
 	import * as THREE from 'three';
 	import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-	import { Sky } from 'three/addons/objects/Sky.js';
-	import { Timer } from 'three/addons/misc/Timer.js';
 	let canvas: HTMLCanvasElement;
 	let lilGuiPlacer: HTMLSpanElement;
 	onMount(() => {
