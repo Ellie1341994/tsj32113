437260cf6942bbac7b35191eca11c5a560a9efbf
Elias <eli1341994@gmail.com>
2024-10-15 01:02:33 -0300

    [Add] Challenges and Project links on root path

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,30 @@
-<!-- <a style="color: black; font-size: 48px;" href="/">TSJ</a> -->
+<div>
+	<h1>
+		<a class="disabled-link" href="/">challenges</a>
+	</h1>
+	<h1><a href="/basics">projects</a></h1>
+</div>
+
+<style lang="scss">
+	div {
+		height: 100%;
+		// width: 100%;
+		display: flex;
+		align-items: center;
+		justify-content: space-around;
+		h1 {
+			font-size: 4em;
+			a {
+				text-decoration: none;
+				text-transform: uppercase;
+				color: var(--color-theme-3);
+				.disabled-link {
+					pointer-events: none;
+				}
+				&:hover:not(.disabled-link) {
+					text-shadow: #70577a 3px 4px 5px;
+				}
+			}
+		}
+	}
+</style>
