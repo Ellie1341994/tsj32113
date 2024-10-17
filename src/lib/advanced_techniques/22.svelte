fabf133f6e8c8189601d2442414bb714abe6545d
Elias <eli1341994@gmail.com>
2024-10-17 14:07:16 -0300

    [Add] lesson 22 initial code

---
+++ src/lib/advanced_techniques/22.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import gsap from 'gsap';
+	import { onMount } from 'svelte';
+	import * as THREE from 'three';
+	import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+	import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+	import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
+	let canvas: HTMLCanvasElement;
+	let lilGuiPlacer: HTMLSpanElement;
+	onMount(() => {
+		alert('hola');
+	});
+</script>
+
+<canvas class="webgl" bind:this={canvas}></canvas>
+<span class="lil-gui-placer" bind:this={lilGuiPlacer}></span>
+
+<style>
+	span.lil-gui-placer {
+		position: absolute;
+		top: 20vh;
+		right: 13.5vw;
+	}
+	canvas {
+		border: none;
+		box-shadow: none;
+	}
+</style>
