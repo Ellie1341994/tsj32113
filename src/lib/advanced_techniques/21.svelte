14aaa435af155fd551c5015cbfee2c8557de5594
Elias <eli1341994@gmail.com>
2024-10-16 23:32:32 -0300

    [Add] lesson 21 code for models and animations

--- src/lib/advanced_techniques/21.svelte
+++ src/lib/advanced_techniques/21.svelte
@@ -1,36 +1,80 @@
 <script lang="ts">
 	import gsap from 'gsap';
-	// import { page } from `$app/stores`;
-	// import gsap from `gsap`;
-	import GUI from 'lil-gui';
 	import { onMount } from 'svelte';
 	import * as THREE from 'three';
 	import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-	// @ts-ignore
-	import * as CANNON from 'cannon-es';
-	import { rendererReference } from 'three/webgpu';
+	import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+	import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
 	let canvas: HTMLCanvasElement;
 	let lilGuiPlacer: HTMLSpanElement;
 	onMount(() => {
 		// Utils
+		let sceneReady = false;
 		const parameters = {
 			width: window.innerWidth * 0.75,
 			height: window.innerHeight * 0.75,
-			color: '#ff3399'
+			color: '#999999'
+			//  '#ff3399'
 		};
 		const ASPECT_RATIO = parameters.width / parameters.height;
-		// Meshes
-		const cube = new THREE.Mesh(
-			new THREE.BoxGeometry(3, 9),
-			new THREE.MeshToonMaterial({ color: parameters.color })
+		const ASSETS_PATH = '/assets/advanced/21/models';
+		// Loader
+		const gltfLoader = new GLTFLoader();
+		// Models
+		const dracoLoader = new DRACOLoader();
+		// dracoLoader.setDecoderPath('/assets/advanced/21/workers/draco/');
+		dracoLoader.setDecoderPath('/assets/advanced/21/workers/draco/');
+		dracoLoader.setDecoderPath('../../node_modules/three/examples/jsm/libs/draco/');
+		gltfLoader.setDRACOLoader(dracoLoader);
+		let mixer: any = null;
+		let actions: any = null;
+		gltfLoader.load(
+			// `${ASSETS_PATH}/Duck/glTF/Duck.gltf`,
+			// `${ASSETS_PATH}/Duck/glTF-Binary/Duck.glb`,
+			// `${ASSETS_PATH}/FlightHelmet/glTF/FlightHelmet.gltf`,
+			// `${ASSETS_PATH}/Duck/glTF-Draco/Duck.gltf`,
+			`${ASSETS_PATH}/Fox/glTF/Fox.gltf`,
+			(gltf) => {
+				// scene.add(gltf.scene.children[0]); //Duck
+				// for (const child of gltf.scene.children) {
+				// 	child.scale.set(0.05, 0.05, 0.05);
+				// }
+				// scene.add(...gltf.scene.children);
+				// const [fox]: any = gltf.scene.children[0].children;
+				// console.log(fox);
+				// fox.material.color.set(new THREE.Color(parameters.color));
+				gltf.scene.scale.set(0.05, 0.05, 0.05);
+				mixer = new THREE.AnimationMixer(gltf.scene);
+				actions = gltf.animations;
+				// const action = mixer.clipAction(gltf.animations[2]);
+				// action.play();
+				scene.add(gltf.scene);
+				sceneReady = true;
+				console.log('loaded');
+			},
+			() => {
+				console.log('loading');
+			},
+			() => {
+				console.log('load error');
+			}
 		);
-		cube.receiveShadow = true;
+		// Meshes
+		const platform = new THREE.Mesh(
+			new THREE.BoxGeometry(10, 0.1, 10),
+			new THREE.MeshStandardMaterial({ color: parameters.color })
+		);
+		platform.receiveShadow = true;
 		// Light
 		const ambientLight = new THREE.AmbientLight('#aaaaaa');
 		ambientLight.castShadow = true;
+		const directionalLight = new THREE.DirectionalLight('#ffffff', 3);
+		directionalLight.shadow.mapSize.set(1024, 1024);
+		directionalLight.position.set(3, 3, 6);
+		const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight);
 		// Scene
 		const scene = new THREE.Scene();
-		scene.add(cube, ambientLight);
+		scene.add(platform, ambientLight, directionalLight);
 		// Camera
 		const camera = new THREE.PerspectiveCamera(75, ASPECT_RATIO);
 		camera.position.set(0, 3, 9);
@@ -38,17 +82,45 @@
 		control.enableDamping = true;
 
 		// Renderer
-		const renderer = new THREE.WebGLRenderer({ canvas });
+		const renderer = new THREE.WebGLRenderer({ canvas, alpha: true });
 		renderer.setSize(parameters.width, parameters.height);
-
 		renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-		// renderer.shadowMap.enabled = true;
 
 		// Play
+		gsap.to(scene.rotation, { y: Math.PI * 2, duration: 15, repeat: -1, yoyo: true });
+		const clock = new THREE.Clock();
+		let previousElapsedTime = clock.getElapsedTime();
+		let prolongatedElapsedTime = clock.getElapsedTime();
+		let animationTriggeringSeconds = 3;
+		let animationIndex = 0;
 		let tickId = 0;
+		let action: any = null;
 		function tick() {
+			const elapsedTime = clock.getElapsedTime();
+			const deltaTime = elapsedTime - previousElapsedTime;
+			let prolongatedDeltaTime = elapsedTime - prolongatedElapsedTime;
+			previousElapsedTime = elapsedTime;
+			// Mixer
+			if (sceneReady) {
+				// action.play();
+				if (prolongatedDeltaTime > animationTriggeringSeconds) {
+					prolongatedElapsedTime = elapsedTime;
+					prolongatedDeltaTime = 0;
+					if (action) {
+						action.stop();
+					}
+					action = mixer.clipAction(actions[animationIndex]);
+					// action.setDuration(animationTriggeringSeconds);
+					action.play();
+
+					animationIndex += animationIndex < 2 ? 1 : -animationIndex;
+				}
+				mixer.update(deltaTime);
+			}
+			// Control
 			control.update();
-			renderer.render(scene, camera);
+
+			sceneReady && renderer.render(scene, camera);
 			tickId = window.requestAnimationFrame(tick);
 		}
 		tick();
