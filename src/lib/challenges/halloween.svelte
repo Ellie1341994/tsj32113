167baf114509287d82a819a4d027ba60926a5cf8
Elias <eli1341994@gmail.com>
2024-10-18 22:27:15 -0300

    [Add] cart and horse animations

--- src/lib/challenges/halloween.svelte
+++ src/lib/challenges/halloween.svelte
@@ -4,10 +4,12 @@
 	import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 	import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 	import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
+	import { mix } from 'three/webgpu';
 	let canvas: HTMLCanvasElement;
 	onMount(() => {
 		// Utils
-		let sceneReady = false;
+		let pumpkinReady = false;
+		let ankouReady = false;
 		const parameters = {
 			width: window.innerWidth * 0.9,
 			height: window.innerHeight * 0.9,
@@ -27,17 +29,18 @@
 		//  Models
 		const ankouModel: any = null;
 		const pumpkinModel: any = null;
+		let ankouAnimations: any = null;
+		let mixer: any = null;
 		// Loaders
 		const gltfLoader = new GLTFLoader();
 		gltfLoader.load(
 			parameters.modelsURL.cart,
-			(gltf) => {
+			(gltfAnoku) => {
 				let recursivelyFindHead = (obj: any) => {
 					if (obj?.children) {
 						const i = obj.children.findIndex(({ name }: any) => {
 							return name === 'head';
 						});
-						console.log('i', i);
 						if (i >= 0) {
 							obj.children[i].scale.set(0.001, 0.001, 0.001);
 							console.log('head removed');
@@ -50,13 +53,16 @@
 						});
 					}
 				};
-				recursivelyFindHead(gltf.scene);
-				scene.add(gltf.scene);
-				sceneReady = true;
+				recursivelyFindHead(gltfAnoku.scene);
+				ankouAnimations = gltfAnoku.animations;
+				mixer = new THREE.AnimationMixer(gltfAnoku.scene);
+				console.log('ankouAnimations', ankouAnimations);
+				scene.add(gltfAnoku.scene);
+				ankouReady = true;
+				// console.log(gltfAnoku);
 			},
 			() => {
 				// console.log('loading');
-				sceneReady = false;
 			},
 			() => {
 				console.log('load error');
@@ -69,22 +75,27 @@
 				gltfPumpkin.scene.position.y = 2.55;
 				scene.add(gltfPumpkin.scene);
 
-				sceneReady = true;
+				pumpkinReady = true;
 			},
 			() => {
 				// console.log('loading');
-				sceneReady = false;
 			},
 			() => {
 				console.log('load error');
 			}
 		);
 		// Meshes
-		const platform = new THREE.Mesh(
+		const platformA = new THREE.Mesh(
 			new THREE.BoxGeometry(10, 0.1, 10),
-			new THREE.MeshStandardMaterial({ color: parameters.color })
+			new THREE.MeshStandardMaterial({ color: parameters.color, wireframe: true })
 		);
-		platform.receiveShadow = true;
+		const platformB = new THREE.Mesh(
+			new THREE.BoxGeometry(10, 0.1, 10),
+			new THREE.MeshStandardMaterial({ color: parameters.color, wireframe: true })
+		);
+		platformA.receiveShadow = true;
+		platformB.receiveShadow = true;
+		platformB.rotation.y = Math.PI * 0.25;
 		// Light
 		const ambientLight = new THREE.AmbientLight('#aaaaaa');
 		ambientLight.castShadow = true;
@@ -94,10 +105,11 @@
 		// const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight);
 		// Scene
 		const scene = new THREE.Scene();
-		scene.add(platform, ambientLight, directionalLight);
+		scene.add(platformA, platformB, ambientLight, directionalLight);
+		scene.rotation.y = -Math.PI * 0.25;
 		// Camera
 		const camera = new THREE.PerspectiveCamera(75, parameters.aspectRatio);
-		camera.position.set(0, 2, 4);
+		camera.position.set(0, 3, 10);
 		const control = new OrbitControls(camera, canvas);
 		control.enableDamping = true;
 
@@ -109,13 +121,24 @@
 		// Play
 		const clock = new THREE.Clock();
 		let tickId = 0;
+		let previousElapsedTime = clock.getElapsedTime();
 		function tick() {
 			const elapsedTime = clock.getElapsedTime();
-
-			// Control
+			const deltaTime = elapsedTime - previousElapsedTime;
+			previousElapsedTime = elapsedTime;
 			control.update();
+			let sceneReady = pumpkinReady && ankouReady;
 
-			sceneReady && renderer.render(scene, camera);
+			if (sceneReady) {
+				// Control
+				let horseWalk = mixer.clipAction(ankouAnimations[2]);
+				horseWalk?.play();
+				let cartMove = mixer.clipAction(ankouAnimations[4]);
+				cartMove.play();
+				// console.log(`A:${action} M:`, mixer);
+				mixer.update(deltaTime);
+			}
+			renderer.render(scene, camera);
 			tickId = window.requestAnimationFrame(tick);
 		}
 		tick();
