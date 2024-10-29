2fbd97bf28dc7880b6a095da61209915ba8f8cc2
Elias <eli1341994@gmail.com>
2024-10-28 21:26:38 -0300

    [Add] Lesson title for lesson >= 3 and single style for all canvas

--- src/routes/[module]/[lesson]/+page.svelte
+++ src/routes/[module]/[lesson]/+page.svelte
@@ -12,27 +12,21 @@
 {:then Scene}
 	{@const title = submodules[lesson - 1]}
 	<svelte:component this={Scene.default} />
-	<span
-		bind:this={titlePlacer}
-		class={`lesson-title-placer ${lesson > 6 ? 'big-canvas' : 'small-canvas'}`}>{title}</span
-	>
-	<style lang="scss">
-		span.lesson-title-placer {
-			position: absolute;
-			&.big-canvas {
+	{#if lesson > 2}
+		<span bind:this={titlePlacer} class={`lesson-title-placer`}>{title}</span>
+
+		<style lang="scss">
+			span.lesson-title-placer {
+				position: absolute;
 				top: 15vh;
 				left: 12.5vw;
+				background-color: var(--color-theme-4);
+				padding: 1vh;
+				// height: 1vh;
+				font-weight: bold;
+				/* border: 10px solid var(--color-theme-4); */
+				border-radius: 0 0 1vh 0;
 			}
-			&.small-canvas {
-				top: calc(52vh - 300px);
-				left: calc(50vw - 400px);
-			}
-			background-color: var(--color-theme-4);
-			padding: 1vh;
-			// height: 1vh;
-			font-weight: bold;
-			/* border: 10px solid var(--color-theme-4); */
-			border-radius: 0 0 1vh 0;
-		}
-	</style>
+		</style>
+	{/if}
 {/await}
