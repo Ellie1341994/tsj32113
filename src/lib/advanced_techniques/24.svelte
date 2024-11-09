797ebf68681648781f6c7b9f71fc7f30e98c7776
Elias <eli1341994@gmail.com>
2024-11-08 23:23:17 -0300

    [Change | Add] FightHelmet asset folder position and textures in lesson 24

--- src/lib/advanced_techniques/24.svelte
+++ src/lib/advanced_techniques/24.svelte
@@ -3,8 +3,9 @@
 	import * as THREE from 'three';
 	import gsap from 'gsap';
 	import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-	import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
 	import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+	import GUI from 'lil-gui';
+
 	let canvas: HTMLCanvasElement;
 	let lilGuiPlacer: HTMLSpanElement;
 	onMount(() => {
@@ -17,23 +18,60 @@
 				return this.width / this.height;
 			}
 		};
+		const gui = new GUI({ title: 'Tweaks panel', width: 300, container: lilGuiPlacer });
+		// Textures
 		// Loader
-		const gltfLoader = new GLTFLoader();
+		const cubeTextureLoader = new THREE.CubeTextureLoader();
+		//  Low Dynamic Range LDR Cube texture
+		const environmentMap = cubeTextureLoader.load([
+			'/assets/advanced/24/environmentMaps/2/px.png',
+			'/assets/advanced/24/environmentMaps/2/nx.png',
+			'/assets/advanced/24/environmentMaps/2/py.png',
+			'/assets/advanced/24/environmentMaps/2/ny.png',
+			'/assets/advanced/24/environmentMaps/2/pz.png',
+			'/assets/advanced/24/environmentMaps/2/nz.png'
+		]);
 		// Meshes
 		const torusKnotMesh = new THREE.Mesh(
 			new THREE.TorusKnotGeometry(1, 0.4, 100, 16),
-			new THREE.MeshBasicMaterial({ color: '#ffffff' })
+			new THREE.MeshStandardMaterial({
+				color: '#ffffff',
+				roughness: 0.3,
+				metalness: 1
+				// envMap: environmentMap
+			})
 		);
-		// Lights
-		const ambientLight = new THREE.AmbientLight('#ffffff');
-		const directionalLight = new THREE.DirectionalLight('#ffffff');
-		directionalLight.castShadow = true;
-		// Scene
+		torusKnotMesh.position.y = -5;
+		// Models
+		const gltfLoder = new GLTFLoader();
+		gltfLoder.load(
+			'/assets/advanced/24/FlightHelmet/glTF/FlightHelmet.gltf',
+			(gltfFlightHelmet) => {
+				gltfFlightHelmet.scene.scale.set(10, 10, 10);
+				scene.add(gltfFlightHelmet.scene);
+			}
+		);
+		// // Lights
+		// const ambientLight = new THREE.AmbientLight('#ffffff');
+		// const directionalLight = new THREE.DirectionalLight('#ffffff');
+		// directionalLight.castShadow = true;
+		// // Scene
 		const scene = new THREE.Scene();
-		scene.add(torusKnotMesh, ambientLight, directionalLight);
+		scene.background = environmentMap;
+		scene.environment = environmentMap;
+		scene.add(torusKnotMesh);
+		gui.add(scene, 'environmentIntensity', 0, 10, 0.01);
+		gui.add(scene, 'backgroundIntensity', 0, 5, 0.01);
+		gui.add(scene, 'backgroundBlurriness', 0, 1, 0.01);
+		gui
+			.add(scene.backgroundRotation, 'y', 0, Math.PI * 2, Math.PI * 0.01)
+			.name('backgroundRotationY');
+		gui
+			.add(scene.environmentRotation, 'y', 0, Math.PI * 2, Math.PI * 0.01)
+			.name('environmentRotationY');
 		// Camera
 		const camera = new THREE.PerspectiveCamera(75, parameters.aspectRatio);
-		camera.position.z = 5;
+		camera.position.set(0, 10, 10);
 		const control = new OrbitControls(camera, canvas);
 		// Renderer
 		const renderer = new THREE.WebGLRenderer({ canvas });
