76fb4f7d1433a18e9e69e0b018b9d9084c364cdd
Elias <eli1341994@gmail.com>
2024-11-12 15:32:25 -0300

    [Add] Rest of the lesson 24

--- src/lib/advanced_techniques/24.svelte
+++ src/lib/advanced_techniques/24.svelte
@@ -5,12 +5,16 @@
 	import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 	import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 	import GUI from 'lil-gui';
+	import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
+	// import { EXRLoader } from 'three/addons/loaders/EXRLoader.js';
+	import { GroundedSkybox } from 'three/addons/objects/GroundedSkybox.js';
 
 	let canvas: HTMLCanvasElement;
 	let lilGuiPlacer: HTMLSpanElement;
 	onMount(() => {
 		// Utils
 		const parameters = {
+			s: 0,
 			width: innerWidth * 0.75,
 			height: innerHeight * 0.75,
 			color: '#997766',
@@ -18,36 +22,101 @@
 				return this.width / this.height;
 			}
 		};
-		const gui = new GUI({ title: 'Tweaks panel', width: 300, container: lilGuiPlacer });
+		const gui = new GUI({
+			title: 'Tweaks panel',
+			width: 300,
+			container: lilGuiPlacer
+		});
+		gui.close();
 		// Textures
+		// Cube render target
+		const cubeRenderTarget = new THREE.WebGLCubeRenderTarget(256, { type: THREE.HalfFloatType });
 		// Loader
 		const cubeTextureLoader = new THREE.CubeTextureLoader();
 		//  Low Dynamic Range LDR Cube texture
-		const environmentMap = cubeTextureLoader.load([
-			'/assets/advanced/24/environmentMaps/2/px.png',
-			'/assets/advanced/24/environmentMaps/2/nx.png',
-			'/assets/advanced/24/environmentMaps/2/py.png',
-			'/assets/advanced/24/environmentMaps/2/ny.png',
-			'/assets/advanced/24/environmentMaps/2/pz.png',
-			'/assets/advanced/24/environmentMaps/2/nz.png'
-		]);
+		// const environmentMap = cubeTextureLoader.load([
+		// 	'/assets/advanced/24/environmentMaps/2/px.png',
+		// 	'/assets/advanced/24/environmentMaps/2/nx.png',
+		// 	'/assets/advanced/24/environmentMaps/2/py.png',
+		// 	'/assets/advanced/24/environmentMaps/2/ny.png',
+		// 	'/assets/advanced/24/environmentMaps/2/pz.png',
+		// 	'/assets/advanced/24/environmentMaps/2/nz.png'
+		// ]);
+
+		// HDRI (RGBE Equirectangular)
+		const rgbeLoader = new RGBELoader();
+		// rgbeLoader.load(
+		// 	'/assets/advanced/24/environmentMaps/customEnvMapLightsBlender-2k.hdr',
+		// 	(environmentMap) => {
+		// 		environmentMap.mapping = THREE.EquirectangularReflectionMapping;
+		// 		scene.background = environmentMap;
+		// 		scene.environment = environmentMap;
+		// 	}
+		// );
+		// HDRI (EXR Equirectangular)
+		// const exrLoader = new EXRLoader();
+		// exrLoader.load('/assets/advanced/24/environmentMaps/nvidiaCanvas-4k.exr', (environmentMap) => {
+		// 	environmentMap.mapping = THREE.EquirectangularReflectionMapping;
+		// 	scene.background = environmentMap;
+		// 	scene.environment = environmentMap;
+		// });
+		const textureLoader = new THREE.TextureLoader();
+		const environmentMap = textureLoader.load(
+			'/assets/advanced/24/environmentMaps/blockadesLabsSkybox/interior_views_cozy_wood_cabin_with_cauldron_and_p.jpg'
+		);
+		// Ground projected skybox
+		const skybox = new GroundedSkybox(environmentMap, 15, 25);
+		skybox.layers.enable(1);
+		skybox.position.y = 15;
+		// gui.add(skybox, 'radius', 1, 200, 0.1).name('skyboxRadius');
+		// gui.add(skybox, 'height', 1, 200, 0.1).name('skyboxHeight');
+		gui.add(skybox.position, 'y', 0, 30, 0.1).name('skyboxPos');
+		environmentMap.mapping = THREE.EquirectangularReflectionMapping;
+		environmentMap.colorSpace = THREE.SRGBColorSpace;
+		//  Real time environment map
+
 		// Meshes
+		const holyDonut = new THREE.Mesh(
+			new THREE.TorusGeometry(21, 1, 12, 6),
+			new THREE.MeshBasicMaterial({ color: new THREE.Color(10, 5, 2) })
+		);
+		holyDonut.layers.enable(1);
+		holyDonut.scale.setScalar(3);
+		holyDonut.rotation.z = Math.PI * 0.5;
+		gsap.to(holyDonut.rotation, {
+			y: 2 * Math.PI,
+			x: 2 * Math.PI,
+			duration: 3.6,
+			repeat: -1,
+			ease: 'linear',
+			animationFillMode: 'forwards'
+		});
+		gui.add(holyDonut, 'visible').name('TorusLightVisibility');
 		const torusKnotMesh = new THREE.Mesh(
 			new THREE.TorusKnotGeometry(1, 0.4, 100, 16),
 			new THREE.MeshStandardMaterial({
 				color: '#ffffff',
-				roughness: 0.3,
+				roughness: 0.1,
 				metalness: 1
 				// envMap: environmentMap
 			})
 		);
-		torusKnotMesh.position.y = -5;
+		torusKnotMesh.position.y = 25;
+		torusKnotMesh.scale.setScalar(3);
+
+		gsap.to(torusKnotMesh.rotation, {
+			y: 2 * Math.PI,
+			duration: 3.6,
+			repeat: -1, // Infinite repeat
+			ease: 'linear',
+			animationFillMode: 'forwards'
+		});
 		// Models
 		const gltfLoder = new GLTFLoader();
 		gltfLoder.load(
 			'/assets/advanced/24/FlightHelmet/glTF/FlightHelmet.gltf',
 			(gltfFlightHelmet) => {
-				gltfFlightHelmet.scene.scale.set(10, 10, 10);
+				gltfFlightHelmet.scene.scale.setScalar(25);
 				scene.add(gltfFlightHelmet.scene);
 			}
 		);
@@ -57,12 +126,14 @@
 		// directionalLight.castShadow = true;
 		// // Scene
 		const scene = new THREE.Scene();
-		scene.background = environmentMap;
-		scene.environment = environmentMap;
-		scene.add(torusKnotMesh);
+		// scene.background = new THREE.MeshStandardMaterial({ color: '#333333' });
+		scene.environment = cubeRenderTarget.texture;
+		scene.add(skybox, torusKnotMesh, holyDonut);
+		// scene.background = environmentMap;
+		// scene.environment = environmentMap
 		gui.add(scene, 'environmentIntensity', 0, 10, 0.01);
-		gui.add(scene, 'backgroundIntensity', 0, 5, 0.01);
-		gui.add(scene, 'backgroundBlurriness', 0, 1, 0.01);
+		// gui.add(scene, 'backgroundIntensity', 0, 5, 0.01);
+		// gui.add(scene, 'backgroundBlurriness', 0, 1, 0.01);
 		gui
 			.add(scene.backgroundRotation, 'y', 0, Math.PI * 2, Math.PI * 0.01)
 			.name('backgroundRotationY');
@@ -70,8 +141,13 @@
 			.add(scene.environmentRotation, 'y', 0, Math.PI * 2, Math.PI * 0.01)
 			.name('environmentRotationY');
 		// Camera
+		// Cube camera
+		const cubeCamera = new THREE.CubeCamera(0.1, 100, cubeRenderTarget);
+		cubeCamera.layers.set(1);
+
+		// Normal
 		const camera = new THREE.PerspectiveCamera(75, parameters.aspectRatio);
-		camera.position.set(0, 10, 10);
+		camera.position.set(0, 35, 75);
 		const control = new OrbitControls(camera, canvas);
 		// Renderer
 		const renderer = new THREE.WebGLRenderer({ canvas });
@@ -84,6 +160,7 @@
 			cancelAnimationFrame(tickId);
 			// Update
 			control.update();
+			cubeCamera.update(renderer, scene);
 			// Render
 			renderer.render(scene, camera);
 			tickId = requestAnimationFrame(tick);
