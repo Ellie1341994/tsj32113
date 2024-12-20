a0520f601f456010828c21262753e60b83e636a2
Elias <eli1341994@gmail.com>
2024-12-20 17:48:25 -0300

    [Add] Root page 3d models and related navigation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,11 @@
 	import { onMount } from 'svelte';
 	import * as THREE from 'three';
 	import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-	import GUI from 'lil-gui';
+	import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
+	import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
+	import { goto } from '$app/navigation';
+	import gsap from 'gsap';
+
 	let canvas: HTMLCanvasElement;
 	onMount(() => {
 		// Utils
@@ -17,11 +21,14 @@
 			color: '#997766',
 			get aspectRatio() {
 				return this.width / this.height;
-			}
+			},
+			cursor: {
+				x: 0,
+				y: 0
+			},
+			intersections: [] as any
 		};
-		const gui = new GUI({
-			title: 'Tweaks panel'
-		});
+
 		const setRendererSize = () => {
 			// console.log('Window size has changed.');
 			// Camera AR update
@@ -33,33 +40,84 @@
 			renderer.render(scene, camera);
 		};
 		addEventListener('resize', setRendererSize);
-		gui.close();
+		addEventListener('mousemove', ({ clientX, clientY }) => {
+			parameters.cursor.x = (clientX / innerWidth) * 2 - 1;
+			parameters.cursor.y = -((clientY / innerHeight) * 2 - 1);
+		});
+		addEventListener('mousedown', ({ button }) => {
+			const leftClick = 0;
+			if (button === leftClick) {
+				const [{ object = { name: '' } } = {}] = parameters?.intersections;
+				/challenges|basics/.test(object.name) && goto(`/${object.name}`);
+				console.log(object);
+			}
+		});
 		// Textures
-		// Loader
-		const textureLoader = new THREE.TextureLoader();
-		// Geometry
-		const geometry = new THREE.TorusGeometry(1, 1, 3, 3);
-		// Materials
-		const material = new THREE.MeshStandardMaterial({ color: '#993333' });
-		// Meshes
-		const sampleMesh = new THREE.Mesh(geometry, material);
-		sampleMesh.receiveShadow = true;
-		sampleMesh.castShadow = true;
 		// Lights
-		const abmbientLight = new THREE.HemisphereLight('#ffffff', 9);
+		const light = new THREE.HemisphereLight('#ffffff', 9);
 		// Scene
 		const scene = new THREE.Scene();
+		// Loader
+		const fontLoader = new FontLoader();
+		fontLoader.load('fonts/optimer_regular.typeface.json', (font) => {
+			console.log('loaded');
+			// Geometries
+			const challengesTextGeometry = new TextGeometry('Challenges', {
+				font,
+				size: 0.5,
+				depth: 0.2,
+				curveSegments: 12,
+				bevelEnabled: true,
+				bevelThickness: 0.03,
+				bevelSize: 0.02,
+				bevelOffset: 0,
+				bevelSegments: 5
+			});
+			challengesTextGeometry.center();
+			const projectsTextGeometry = new TextGeometry('[#]Projects', {
+				font,
+				size: 0.5,
+				depth: 0.2,
+				curveSegments: 12,
+				bevelEnabled: true,
+				bevelThickness: 0.03,
+				bevelSize: 0.02,
+				bevelOffset: 0,
+				bevelSegments: 5
+			});
+			projectsTextGeometry.center();
+			// Materials
+			const challengesTextMaterial = new THREE.MeshStandardMaterial({
+				color: '#ff6699'
+				// metalness: 0.3
+				// roughness: 0.9
+			});
+			const projectsTextMaterial = new THREE.MeshStandardMaterial({
+				color: '#ff6699',
 
-		scene.add(abmbientLight, sampleMesh);
+				wireframe: false
+			});
+			// Meshes
+			const challengesText = new THREE.Mesh(challengesTextGeometry, challengesTextMaterial);
+			challengesText.name = 'challenges';
+			const projectsText = new THREE.Mesh(projectsTextGeometry, projectsTextMaterial);
+			projectsText.name = 'basics';
+			// Params
+			challengesText.position.x = -2;
+			projectsText.position.x = 2;
+			challengesText.rotation.y = Math.PI * 0.15;
+			projectsText.rotation.y = -challengesText.rotation.y;
+			scene.add(challengesText, projectsText);
+		});
+		scene.add(light);
+		// Raycaster
+		const raycaster = new THREE.Raycaster();
 		// Camera
-		// Cube camera
-		// Normal
 		const camera = new THREE.PerspectiveCamera(75, parameters.aspectRatio);
 		camera.position.set(0, 0, 4);
 		const control = new OrbitControls(camera, canvas);
 		// Renderer
-		const renderer = new THREE.WebGLRenderer({ canvas });
-		renderer.setClearColor('#11111ff', 0);
+		const renderer = new THREE.WebGLRenderer({ canvas, alpha: true });
 		renderer.shadowMap.enabled = true;
 		renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
@@ -69,11 +127,36 @@
 		// Play
 		let tickId = 0;
 		const clock = new THREE.Clock();
+		let currentHoveredObj: any = undefined;
+		let scalingAnimationOn = false;
 		function tick() {
 			const elapsedTime = clock.getElapsedTime();
 			cancelAnimationFrame(tickId);
 			// Update
 			control.update();
+			// Intersect
+			const { x, y } = parameters.cursor;
+			const pointer = new THREE.Vector2(x, y);
+			raycaster.setFromCamera(pointer, camera);
+			parameters.intersections = raycaster.intersectObjects(scene.children);
+			if (!scalingAnimationOn) {
+				if (parameters.intersections.length && !currentHoveredObj) {
+					const [{ object }] = parameters?.intersections;
+					currentHoveredObj = object;
+					scalingAnimationOn = true;
+					gsap.to(currentHoveredObj.scale, { x: 1.25, y: 1.25, z: 1.25, duration: 1 }).then(() => {
+						scalingAnimationOn = false;
+					});
+				}
+				if (!parameters.intersections.length && currentHoveredObj) {
+					scalingAnimationOn = true;
+
+					gsap.to(currentHoveredObj.scale, { x: 1, y: 1, z: 1, duration: 1 }).then(() => {
+						scalingAnimationOn = false;
+					});
+					currentHoveredObj = undefined;
+				}
+			}
 			// Render
 			renderer.render(scene, camera);
 			tickId = requestAnimationFrame(tick);
